ab6aa007c2b0467d776648de548e32c9eb471979
Petr Simcak <psimcak@c1r1s4.42prague.com>
2023-09-01 18:05:02 +0200

    char to unsigned char

--- 01_LIBFT/libft/ft_strncmp.c
+++ 01_LIBFT/libft/ft_strncmp.c
@@ -17,11 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned long	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	while (((unsigned char)s1[i] || (unsigned char)s2[i]) && i < n)
 	{
-		if (s1[i] < s2[i])
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
 			return (-1);
-		else if (s1[i] > s2[i])
+		else if ((unsigned char)s1[i] > (unsigned char)s2[i])
 			return (1);
 		i++;
 	}
