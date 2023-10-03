11f9827fadaa75df46617ec0819fce37987da6b1
Petr Simcak <78808411+simcak@users.noreply.github.com>
2023-10-03 18:25:14 +0200

    hw

--- 05_PIPEX/pipex/src/pipex.c
+++ 05_PIPEX/pipex/src/pipex.c
@@ -6,59 +6,47 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:17:14 by psimcak           #+#    #+#             */
-/*   Updated: 2023/09/28 18:57:59 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/10/03 18:16:11 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	child_process(char **argv, char **envp, int *fd)
+void	ft_child_process(char *argv[], char **envp, int *fd)
 {
-	int		filein;
+	char	**splited_argv;
+	int		i;
 
-	filein = open(argv[1], O_RDONLY, 0444);
-	if (filein == -1)
-		ft_error("Error with OPEN");
-	dup2(fd[1], STDOUT_FILENO);
-	dup2(filein, STDIN_FILENO);
 	close(fd[0]);
-	execute(argv[2], envp);
-}
-
-void	parent_process(char **argv, char **envp, int *fd)
-{
-	int		fileout;
-
-	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	if (fileout == -1)
-		ft_error("Error with OPEN");
-	dup2(fd[0], STDIN_FILENO);
-	dup2(fileout, STDOUT_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
-	execute(argv[3], envp);
+	printf("we are here");
+	// splited_argv = ft_split(argv[2], ' ');
+	// execve("/nfs/homes/psimcak/bin/ls", splited_argv, envp);
 }
 
-int	main(int argc, char **argv, char **envp)
+void	ft_parent_process(char *argv[], char **envp, int *fd)
+{
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	execlp("grep", "grep", "rtt", NULL);
+}
+
+int	main(int argc, char *argv[], char **envp)
 {
 	int		fd[2];
-	pid_t	id_1;
+	pid_t	pid_1;
 
-	if (argc == 5)
-	{
-		if (pipe(fd) == -1)
-			ft_error("Error with PIPE");
-		id_1 = fork();
-		if (id_1 == -1)
-			ft_error("Error with FORK");
-		if (id_1 == 0)
-			child_process(argv, envp, fd);
-		waitpid(id_1, NULL, 0);
-		parent_process(argv, envp, fd);
-	}
-	else
-	{
-		ft_putstr_fd("Error: Bad arguments\n", 2);
-		ft_putstr_fd("Expected: ./pipex file1 \"cmd1\" \"cmd2\" file2\n", 1);
-	}
+	if (pipe(fd) == -1)
+		ft_error("Error with PIPE");
+	pid_1 = fork();
+	if (pid_1 == -1)
+		ft_error("Error with 1st FORK");
+	if (pid_1 == 0)
+		ft_child_process(argv, envp, fd);
+	waitpid(pid_1, NULL, 0);
+	ft_parent_process(argv, envp, fd);
+
 	return (0);
 }
--- 05_PIPEX/pipex/src/utils.c
+++ 05_PIPEX/pipex/src/utils.c
@@ -6,60 +6,60 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 18:01:08 by psimcak           #+#    #+#             */
-/*   Updated: 2023/09/28 19:00:08 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/10/03 17:43:04 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-char	*find_path(char *cmd, char **envp)
-{
-	char	**paths;
-	char	*path;
-	int		i;
-	char	*part_path;
+// char	*ft_find_path(char *cmd, char **envp)
+// {
+// 	char	**paths;
+// 	char	*path;
+// 	int		i;
+// 	char	*part_path;
 
-	i = 0;
-	while (ft_strnstr(envp[i], "PATH", 4) == 0)
-		i++;
-	paths = ft_split(envp[i] + 5, ':');
-	i = 0;
-	while (paths[i])
-	{
-		part_path = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(part_path, cmd);
-		free(part_path);
-		if (access(path, F_OK) == 0)
-			return (path);
-		free(path);
-		i++;
-	}
-	i = -1;
-	while (paths[++i])
-		free(paths[i]);
-	free(paths);
-	return (0);
-}
+// 	i = 0;
+// 	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+// 		i++;
+// 	paths = ft_split(envp[i] + 5, ':');
+// 	i = 0;
+// 	while (paths[i])
+// 	{
+// 		part_path = ft_strjoin(paths[i], "/");
+// 		path = ft_strjoin(part_path, cmd);
+// 		free(part_path);
+// 		if (access(path, F_OK) == 0)
+// 			return (path);
+// 		free(path);
+// 		i++;
+// 	}
+// 	i = -1;
+// 	while (paths[++i])
+// 		free(paths[i]);
+// 	free(paths);
+// 	return (0);
+// }
 
-void	execute(char *argv, char **envp)
-{
-	char	**cmd;
-	int 	i;
-	char	*path;
+// void	ft_execute(char *argv, char **envp)
+// {
+// 	char	**cmd;
+// 	int 	i;
+// 	char	*path;
 	
-	i = 0;
-	cmd = ft_split(argv, ' ');
-	path = ft_find_path(cmd[0], envp);
-	if (!path)	
-	{
-		while (cmd[i])
-			free(cmd[i++]);
-		free(cmd);
-		ft_error("Error with PATH");
-	}
-	if (execve(path, cmd, envp) == -1)
-		ft_error("EXECVE failed");
-}
+// 	i = 0;
+// 	cmd = ft_split(argv, ' ');
+// 	path = ft_find_path(cmd[0], envp);
+// 	if (!path)	
+// 	{
+// 		while (cmd[i])
+// 			free(cmd[i++]);
+// 		free(cmd);
+// 		ft_error("Error with PATH");
+// 	}
+// 	if (execve(path, cmd, envp) == -1)
+// 		ft_error("EXECVE failed");
+// }
 
 void	ft_error(char *str)
 {
