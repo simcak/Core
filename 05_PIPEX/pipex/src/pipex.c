c7cf4a766bec37943639d09e1e5987b1d91600f6
Petr Simcak <psimcak@c3r1s3.42prague.com>
2023-10-06 17:12:40 +0200

    mistakes corrected. only TO

--- 05_PIPEX/pipex/src/pipex.c
+++ 05_PIPEX/pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:17:14 by psimcak           #+#    #+#             */
-/*   Updated: 2023/10/05 19:08:24 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/10/06 17:10:37 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ void	ft_child_process(char *argv[], char **envp, int *fd)
 	close(fd[1]);
 	close(fd_in);
 	execve(path, splited_argv, envp);
+	ft_free_array_of_strings(splited_argv);
+	free(path);
 	ft_error("Error");
 }
 
@@ -59,37 +61,46 @@ void	ft_parent_process(char *argv[], char **envp, int *fd)
 	close(fd[0]);
 	close(fd_out);
 	execve(path, splited_argv, envp);
+	ft_free_array_of_strings(splited_argv);
+	free(path);
 	ft_error("Error");
 }
 
-void	ft_error_police(char *argv[], char **envp)
+int	ft_error_police(char *argv, char **envp)
 {
 	char	**splited_paths;
-	char	**splited_argv_2;
-	char	**splited_argv_3;
+	char	**splited_argv;
 	char	*all_paths;
-	char	*path_2;
-	char	*path_3;
+	char	*path;
 
 	all_paths = ft_find_paths(envp);
-	splited_argv_2 = ft_split(argv[2], ' ');
-	splited_argv_3 = ft_split(argv[3], ' ');
+	splited_argv = ft_split(argv, ' ');
 	splited_paths = ft_split(all_paths, ':');
-	path_2 = ft_final_path(splited_paths, splited_argv_2);
-	path_3 = ft_final_path(splited_paths, splited_argv_3);
+	path = ft_final_path(splited_paths, splited_argv);
 	ft_free_array_of_strings(splited_paths);
-	ft_free_array_of_strings(splited_argv_2);
-	ft_free_array_of_strings(splited_argv_3);
+	ft_free_array_of_strings(splited_argv);
+	if (path == 0)
+	{
+		if (path != NULL)
+			free(path);
+		return (0);
+	}
+	free(path);
+	return (1);
 }
 
 int	main(int argc, char *argv[], char **envp)
 {
 	int		fd[2];
 	pid_t	pid_1;
+	int		i;
 
 	if (argc == 5)
 	{
-		// ft_error_police(argv, envp);
+		i = 3;
+		while (i > 1)
+			if (ft_error_police(argv[i--], envp) == 0)
+				ft_error("ERERRORO: command not found");
 		if (pipe(fd) == -1)
 			ft_error("Error with PIPE");
 		pid_1 = fork();
@@ -101,9 +112,6 @@ int	main(int argc, char *argv[], char **envp)
 		ft_parent_process(argv, envp, fd);
 	}
 	else
-	{
-		ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m", 2);
-		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
-	}
-	return (0);
+		ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m\n", 2);
+	return (127);
 }
