7f5d244fc1d4155b32ad5b2b258c2bac334eadff
Petr Simcak <78808411+simcak@users.noreply.github.com>
2023-10-06 19:39:37 +0200

    kosmeticke upravy

--- 05_PIPEX/pipex/src/utils.c
+++ 05_PIPEX/pipex/src/utils.c
@@ -6,49 +6,11 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 18:01:08 by psimcak           #+#    #+#             */
-/*   Updated: 2023/10/06 16:51:43 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/10/06 19:38:50 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-
-char	*ft_find_paths(char **envp)
-{
-	char	*path;
-	int		i;
-
-	i = 0;
-	while (envp[i] != NULL)
-	{
-		if (ft_strnstr(envp[i], "PATH=", 5))
-		{
-			path = envp[i] + 5;
-			return (path);
-		}
-		i++;
-	}
-	return (0);
-}
-
-char	*ft_final_path(char **splited_paths, char **cmd)
-{
-	char	*path;
-	char	*part_path;
-	int		i;
-
-	i = 0;
-	while (splited_paths[i])
-	{
-		part_path = ft_strjoin(splited_paths[i], "/");
-		path = ft_strjoin(part_path, cmd[0]);
-		free(part_path);
-		if (access(path, F_OK) == 0)
-			return (path);
-		free(path);
-		i++;
-	}
-	return (NULL);
-}
 
 void	ft_free_array_of_strings(char **str)
 {
@@ -61,6 +23,29 @@ void	ft_free_array_of_strings(char **str)
 		i++;
 	}
 	free(str);
+}
+
+int	ft_error_police(char *argv, char **envp)
+{
+	char	**splited_paths;
+	char	**splited_argv;
+	char	*all_paths;
+	char	*path;
+
+	all_paths = ft_find_paths(envp);
+	splited_argv = ft_split(argv, ' ');
+	splited_paths = ft_split(all_paths, ':');
+	path = ft_final_path(splited_paths, splited_argv);
+	ft_free_array_of_strings(splited_paths);
+	ft_free_array_of_strings(splited_argv);
+	if (path == 0)
+	{
+		if (path != NULL)
+			free(path);
+		return (0);
+	}
+	free(path);
+	return (1);
 }
 
 void	ft_error(char *str)
