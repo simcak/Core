dfea7e5708327eac592b2a8fab736e59fa024284
Petr Simcak <psimcak@c1r1s4.42prague.com>
2023-10-21 15:56:46 +0200

    list utils

--- 06_PUSH_SWAP/push_swap/src/ft_lst_utils.c
+++ 06_PUSH_SWAP/push_swap/src/ft_lst_utils.c
@@ -6,13 +6,58 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:55:03 by psimcak           #+#    #+#             */
-/*   Updated: 2023/10/18 16:43:20 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/10/21 15:53:59 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_create_list(t_node **a, char **argv)
+t_node	*ft_find_last_node(t_node *list_head)
 {
-	
+	if (list_head == NULL)
+		return (NULL);
+	while (list_head->next)
+		list_head = list_head->next;
+	return (list_head);
+}
+
+void	ft_crate_append_node(t_node **list, int num)
+{
+	t_node	*node;
+	t_node	*last_node;
+
+	if (list == NULL)
+		return ;
+	node = malloc(sizeof(t_node));
+	if (node == NULL)
+		return ;
+	node->value = num;
+	node->next = NULL;
+	if (*list == NULL)
+	{
+		*list = node;
+		node->prev = NULL;
+	}
+	else
+	{
+		last_node = ft_find_last_node(*list);
+		last_node->next = node;
+		node->prev = last_node;
+	}
+}
+
+void	ft_create_list(t_node **list, char **argv)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			exit(1);
+		ft_crate_append_node(list, (int)nbr);
+		i++;
+	}
 }
