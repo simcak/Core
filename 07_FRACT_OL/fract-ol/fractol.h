78ff8f86c1c09b1a909514c25d7dec4d15cc1f6b
Petr Simcak <78808411+simcak@users.noreply.github.com>
2023-11-19 20:45:47 +0100

    Nov 19 work

--- 07_FRACT_OL/fract-ol/fractol.h
+++ 07_FRACT_OL/fract-ol/fractol.h
@@ -6,63 +6,107 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:25:08 by psimcak           #+#    #+#             */
-/*   Updated: 2023/11/17 20:22:02 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/11/19 19:43:15 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-#include "minilibx-linux/mlx.h"
-#include <stdio.h> // printf
-#include <stdlib.h> // malloc free
-#include <unistd.h> // write, constands
-#include <math.h>
-#include <X11/X.h>
-#include <X11/keysym.h>
+# include "minilibx-linux/mlx.h"
+# include <stdio.h> // printf
+# include <stdlib.h> // malloc free
+# include <unistd.h> // write, constands
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
 
-#define ERROR_MESSAGE \
-"Please enter: \t\"./fractol mandelbrot\" \n\t\tOR \
-\n\t\t\"./fractol julia <value_1> <value_2>\"\n"
+# define ERROR_ARGV \
+"Please enter: \t./fractol mandelbrot \n\t\tOR \
+\n\t\t./fractol julia <value_1> <value_2>\n"
+# define ERROR_MALLOC \
+"Problems with malloc()"
 
 // WINDOW SIZE
-#define WIDTH	900
-#define HEIGHT	900
+# define WIDTH	900
+# define HEIGHT	900
 
 // COLORS
 // basic ones
-#define BLACK       0x000000  // RGB(0, 0, 0)
-#define WHITE       0xFFFFFF  // RGB(255, 255, 255)
-#define RED         0xFF0000  // RGB(255, 0, 0)
-#define GREEN       0x00FF00  // RGB(0, 255, 0)
-#define BLUE        0x0000FF  // RGB(0, 0, 255)
+# define BLACK				0x000000	// RGB(0, 0, 0)
+# define WHITE				0xFFFFFF	// RGB(255, 255, 255)
+# define RED				0xFF0000	// RGB(255, 0, 0)
+# define GREEN				0x00FF00	// RGB(0, 255, 0)
+# define BLUE				0x0000FF	// RGB(0, 0, 255)
 // cool ones
-#define DEEP_SPACE_SPARKLE	0x4A646C
-#define ELECTRIC_BLUE		0x7DF9FF
-#define NEON_GREEN			0x39FF14
-#define ELECTRIC_PURPLE		0xBF00FF
-#define CYBER_GRAPE			0x58427C
-#define TURQUOISE_BLUE		0x00FFEF
-#define PHTHALO_BLUE		0x000F89
-#define HOT_MAGENTA			0xFF1DCE
-#define NEON_FUCHSIA		0xFE4164
-#define OUTRAGEOUS_ORANGE	0xFF6E4A
+# define DEEP_SPACE_SPARKLE	0x4A646C
+# define ELECTRIC_BLUE		0x7DF9FF
+# define NEON_GREEN			0x39FF14
+# define ELECTRIC_PURPLE	0xBF00FF
+# define CYBER_GRAPE		0x58427C
+# define TURQUOISE_BLUE		0x00FFEF
+# define PHTHALO_BLUE		0x000F89
+# define HOT_MAGENTA		0xFF1DCE
+# define NEON_FUCHSIA		0xFE4164
+# define OUTRAGEOUS_ORANGE	0xFF6E4A
 
 // STRUCTURES
-typedef struct	s_complex
+typedef struct s_complex
 {
-	double	Re;
-	double	Im;
+	double		re;
+	double		im;
 }				t_complex;
 
-typedef struct	s_fractol
+typedef struct s_img
 {
-	
+	void		*img_ptr;
+	char		*pxl_ptr;
+	int			bpp;
+	int			line_len;
+	int			endian;
+}				t_img;
+
+/*
+typedef struct	s_img
+{
+	void		*img_ptr;	// pointer to the image struct
+	char		*pxl_ptr;	// pointer to the first pixel
+	int			bpp;		// how many bits are in pixel
+	int			line_len;	// length of one line in bytes
+	int			endian;		// marks order of bytes
+}				t_img;
+*/
+
+typedef struct s_fractol
+{
+	char		*name;
+
+	void		*mlx;
+	void		*mlx_win;
+
+	t_img		img;
+
+	double		edge;
+	int			iteration_threshold;
 }				t_fractol;
 
 // PROTOTYPES
-// utils
-int	fractol_strncmp(const char *s1, const char *s2, int n);
-int	ft_strlen(const char *str);
+// string utils
+int			fractol_strncmp(const char *s1, const char *s2, int n);
+int			ft_strlen(const char *str);
+
+// math utils
+double		resize(double k, double new_min, double new_max, double old_max);
+t_complex	ft_sum_complex(t_complex z, t_complex c);
+t_complex	ft_sqr_complex(t_complex z);
+
+// police
+void		ft_error_exit(char *error_message);
+
+// fractal init
+void		fractal_init(t_fractol *fractal);
+
+// fractal render
+void		fractal_render(t_fractol *fractal, int width, int height);
 
 #endif
--- 07_FRACT_OL/fract-ol/main.c
+++ 07_FRACT_OL/fract-ol/main.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:52:33 by psimcak           #+#    #+#             */
-/*   Updated: 2023/11/17 19:32:28 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/11/19 17:59:50 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,27 @@
 
 int	main(int argc, char *argv[])
 {
+	t_fractol	fractal;
+
 	if (argc == 2 && fractol_strncmp(argv[1], "mandelbrot", 10)
 		|| argc == 4 && fractol_strncmp(argv[1], "julia", 5))
 	{
-		return (1);
+		fractal.name = argv[1];
+		fractal_init(&fractal);
+		fractal_render(&fractal, WIDTH, HEIGHT);
+		mlx_loop(fractal.mlx);
 	}
 	else
-	{
-		write(STDERR_FILENO, ERROR_MESSAGE, ft_strlen(ERROR_MESSAGE));
-		exit(EXIT_FAILURE);
-	}
+		ft_error_exit(ERROR_ARGV);
 }
 
 /*
-`mlx_init` is like starting the engine of your car before you can drive. When you call `mlx_init`, you're getting everything ready to create windows and draw graphics on the screen. It sets up the connection between your program and the graphics system of your computer. If `mlx_init` works, it gives you a "key" (in programming, we call this a pointer) that you use to control the graphics you want to create. If it doesn't work, it means something went wrong, and you can't start drawing.
+`mlx_init` is like starting the engine of your car before you can drive. 
+When you call `mlx_init`, you're getting everything ready to create windows 
+	and draw graphics on the screen. 
+It sets up the connection between your program and the graphics system of 
+	your computer. 
+If `mlx_init` works, it gives you a "key" (a pointer) that you use to control
+	the graphics you want to create.
+If it doesn't work, it means something went wrong, and you can't start drawing.
 */
