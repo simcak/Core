bba3daef7cd358cb5a15083bcb0579f68eb6cbe5
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-01-04 15:32:20 +0100

    exam in school prep

--- EXAM_02/Level 1/rot_13/my_one.c
+++ EXAM_02/Level 1/rot_13/my_one.c
@@ -9,12 +9,18 @@ int	main(int argc, char **argv)
 		i = 0;
 		while (argv[1][i])
 		{
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'm')
 				argv[1][i] += 13;
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			else if (argv[1][i] >= 'n' && argv[1][i] <= 'z')
+				argv[1][i] -= 13;
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'M')
 				argv[1][i] += 13;
+			else if (argv[1][i] >= 'N' && argv[1][i] <= 'Z')
+				argv[1][i] -= 13;
+			write(1, &argv[1][i], 1);
+			i++;
 		}
-		i++;
+		write(1, "\n", 1);
 	}
 	return (0);
 }
