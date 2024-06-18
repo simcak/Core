6f55901b82531586751117f5b1fde49fe6c3bdda
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-06-18 17:13:42 +0200

    first edition

--- 08_PHILOSOPHERS/include/philosophers.h
+++ 08_PHILOSOPHERS/include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 14:14:21 by psimcak           #+#    #+#             */
-/*   Updated: 2024/06/18 14:18:01 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/06/18 17:05:47 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,31 @@
 # define PHILOSOPHERS_H
 
 //********** LIBRARIES ***********//
-# include "stdio.h"
-# include "unistd.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 //********** STRUCTURES **********//
+typedef struct	s_global_rules
+{
+	int						number_of_philosophers;
+	int						time_to_die;
+	int						time_to_eat;
+	int						time_to_sleep;
+}	t_global_rules;
+
+typedef struct	s_philosopher
+{
+	int						id;
+	int						time_to_die;
+	int						time_to_eat;
+	int						time_to_sleep;
+	struct s_philosopher	*next;
+}	t_philosopher;
 
 //********** PROTOTYPES **********//
+// LIBFT_PHILO
+int		ft_atoi(const char *str);
+int		ft_strlen(char *str);
 
 #endif
