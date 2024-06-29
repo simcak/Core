7ae4de1cc9f01d7f7f78702d8b2e33c853172e60
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-06-29 17:37:39 +0200

    start dinner 1.0.

--- 08_PHILOSOPHERS/src/DINER_FUNCTIONS/2_start_dinner.c
+++ 08_PHILOSOPHERS/src/DINER_FUNCTIONS/2_start_dinner.c
@@ -6,16 +6,49 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 14:11:05 by psimcak           #+#    #+#             */
-/*   Updated: 2024/06/29 14:20:55 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/06/29 17:36:45 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philosophers.h"
 
-// int	start_dinner(t_dinner *dinner)
-// {
-// 	int	i;
+void	*dining(void *data)
+{
+	t_philos	*philo;
+	t_dinner	*dinner;
 
-// 	i = -1;
-// 	return (SUCCESS);
-// }
+	philo = (t_philos *)data;
+	dinner = philo->dinner;
+// 	wait_before_start(dinner); // TODO
+// 	while (!dinner_finished(dinner))
+// 	{
+// 		if (philo_eat(philo))	// TODO
+// 			return (NULL);
+// 		if (philo_sleep(philo))	// TODO
+// 			return (NULL);
+// 		if (philo_think(philo))	// TODO
+// 			return (NULL);
+// 	}
+	return (NULL);
+}
+
+/**
+ * 
+ */
+int	start_dinner(t_dinner *dinner)
+{
+	int	i;
+
+	if (dinner->num_of_philos == 1)
+		; // TODO
+	i = -1;
+	while (++i < dinner->num_of_philos)
+		if (safe_thread(&dinner->philos[i].thread_id, CREATE, dining, &dinner->philos[i]))
+			return (FAILURE);
+	// all_philos_created(dinner); // TODO
+	i = -1;
+	while (++i < dinner->num_of_philos)
+		if (safe_thread(&dinner->philos[i].thread_id, JOIN, NULL, NULL))
+			return (FAILURE);
+	return (SUCCESS);
+}
--- 08_PHILOSOPHERS/src/LIB_PHILO/utils.c
+++ 08_PHILOSOPHERS/src/LIB_PHILO/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   even_odd.c                                         :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/29 13:46:40 by psimcak           #+#    #+#             */
-/*   Updated: 2024/06/29 14:22:27 by psimcak          ###   ########.fr       */
+/*   Created: 2024/06/29 16:39:05 by psimcak           #+#    #+#             */
+/*   Updated: 2024/06/29 16:39:39 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,17 @@ int	philo_id_is(t_philos *philo)
 	if (philo->id % 2 == 1)
 		return (ODD);
 	return (FAILURE);
+}
+
+/**
+ * @brief Returns the length of the string
+*/
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		;
+	return (i);
 }
