2de9fc99696c8c147ceadd755dd921cacb2f127d
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-09-13 14:11:00 +0200

    accuall replace implemented + comments

--- 09_CPP/Module_01/ex04/Replace.cpp
+++ 09_CPP/Module_01/ex04/Replace.cpp
@@ -6,13 +6,13 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 11:43:44 by psimcak           #+#    #+#             */
-/*   Updated: 2024/09/13 13:30:54 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/09/13 14:10:23 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replace.hpp"
 
-static void	input_file_error(std::ifstream &file, std::string _inFile) {
+static void	safe_input_open(std::ifstream &file, std::string _inFile) {
 	file.open(_inFile);
 	if (!file.is_open()) {
 		std::cout << BR << "Error: input file not found" << RST << std::endl;
@@ -21,7 +21,7 @@ static void	input_file_error(std::ifstream &file, std::string _inFile) {
 	return ;
 }
 
-static void	output_file_error(std::ofstream &file, std::string _outFile) {
+static void	safe_output_open(std::ofstream &file, std::string _outFile) {
 	file.open(_outFile);
 	if (!file.is_open()) {
 		std::cout << BR << "Error: output file not found" << RST << std::endl;
@@ -31,6 +31,28 @@ static void	output_file_error(std::ofstream &file, std::string _outFile) {
 }
 
 /**
+ * 1. ERASE
+ * - erase() func removes the characters in the range [pos, pos+len)
+ * 2. INSERT
+ * - insert() func inserts the characters of the string _replace into the current string
+ */
+void	accual_replace(std::string &line, size_t pos, size_t len, std::string _replace) {
+	line.erase(pos, len);
+	line.insert(pos, _replace);
+	return ;
+}
+
+/**
+ * 1. SAFE GUARDS
+ * - those functions OPEN the files and check their existence
+ * 2. OUTER WHILE LOOP
+ * - getline() func reads the file line by line
+ * - after inner while loop, the line is written to the output file
+ * 3. INNER WHILE LOOP
+ * - find() func searches substring _search in the current line
+ * - pos is actualized on while line and also at the end of the loop
+ *   - if it does't find a match, it returns std::string::npos
+ *   - if it match, the line is modified by accual_replace() func
  */
 void	Replace::myReplace (void) {
 	std::ifstream		input;
@@ -39,12 +61,12 @@ void	Replace::myReplace (void) {
 	std::string			newLine;
 	size_t				pos;
 
-	input_file_error(input, _inFile);
-	output_file_error(output, _outFile);
+	safe_input_open(input, _inFile);
+	safe_output_open(output, _outFile);
 	while (std::getline(input, line)) {
 		pos = 0;
 		while ((pos = line.find(_search, pos)) != std::string::npos) {
-			line.replace(pos, _search.length(), _replace);
+			accual_replace(line, pos, _search.length(), _replace);
 			pos += _replace.length();
 		}
 		output << line << std::endl;
@@ -54,6 +76,7 @@ void	Replace::myReplace (void) {
 	return ;
 }
 
+// Constructor
 Replace::Replace( std::string filename, std::string search, std::string replace ) {
 	_inFile = filename;
 	_outFile = filename + ".replace";
@@ -61,6 +84,7 @@ Replace::Replace( std::string filename, std::string search, std::string replace 
 	_replace = replace;
 }
 
+// Destructor
 Replace::~Replace( void ) {}
 
 /* ***************************************************************  Replace.cpp */
