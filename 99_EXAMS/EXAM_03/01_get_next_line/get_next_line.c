d9e8422273afc5ed698cea868e51c58c4fa1cc37
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-09-19 17:46:10 +0200

    updated version

--- 99_EXAMS/EXAM_03/01_get_next_line/get_next_line.c
+++ 99_EXAMS/EXAM_03/01_get_next_line/get_next_line.c
@@ -1,33 +1,49 @@
 #include "get_next_line.h"
 
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*get_next_line(int fd)
 {
-	char	*str_buf;
-	int		i = 0;
-	int		byte;
-	char	c;
+	static char	buffer[BUFFER_SIZE];
+	char		line[70000];
+	static int	buffer_read;
+	static int 	buffer_pos;
+	int			i;
 
-	if (fd < 0 || BUFFER_SIZE < 1)						// Check if the file descriptor is valid
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
-	str_buf = (char *)malloc(42000000);					// Allocate memory for the buffer
-	byte = read(fd, &c, 1);								// Read the first byte from the file descriptor
-
-	while (byte > 0)									// While the read was successful
+	while (1)
 	{
-		str_buf[i++] = c;								// Store the byte in the return string buffer
-		if (c == EOF || c == '\n')						// If the byte is the end of file or a newline character
-			break ;										// Break the loop
-		byte = read(fd, &c, 1);							// Read the next byte
+		if (buffer_pos >= buffer_read)
+		{
+			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_read <= 0)
+				break ;
+		}
+		line[i++] = buffer[buffer_pos++];
+		if (line[i - 1] == '\n')
+			break ;
 	}
-
-	if (i == 0 || byte < 0)								// Check if there is something in the file || the read was successful
-	{
-		free(str_buf);									// Free the allocated memory if not
-		return (NULL);									// And return NULL
-	}
-	str_buf[i] = '\0';									// It must be null-terminated
-	return (str_buf);									// Return the string buffer
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
 }
 
 int	main(void)
