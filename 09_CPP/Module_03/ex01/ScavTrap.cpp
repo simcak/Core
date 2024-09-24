a8b11bcc0f063c2567e0caa80e56b32d84e13b93
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-09-24 19:54:51 +0200

    module 3 update

--- 09_CPP/Module_03/ex01/ScavTrap.cpp
+++ 09_CPP/Module_03/ex01/ScavTrap.cpp
@@ -6,7 +6,33 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 19:30:11 by psimcak           #+#    #+#             */
-/*   Updated: 2024/09/24 19:30:12 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/09/24 19:52:29 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "ScavTrap.hpp"
+
+/* ****************** CONSTRACTORS ****************** */
+ScavTrap::ScavTrap() : ClapTrap() {
+	std::cout << BG << "ScavTrap default constructor called" << RST << std::endl;
+}
+
+ScavTrap::ScavTrap( const std::string &name ) : ClapTrap( name ) {
+	std::cout << BG << "ScavTrap constructor called" << RST << std::endl;
+}
+
+ScavTrap::ScavTrap( ScavTrap &scavTrap ) {
+	std::cout << BG << "ScavTrap copy constructor called" << RST << std::endl;
+	*this = scavTrap;
+}
+
+/* **************** MEMBER FUNCTIONS **************** */
+void	ScavTrap::attack( std::string const &target ) {
+}
+
+void	ScavTrap::guardGate( void ) {
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << BR << "ScavTrap destructor called" << RST << std::endl;
+}
--- 09_CPP/Module_03/ex01/ScavTrap.hpp
+++ 09_CPP/Module_03/ex01/ScavTrap.hpp
@@ -6,7 +6,29 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 19:30:11 by psimcak           #+#    #+#             */
-/*   Updated: 2024/09/24 19:31:05 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/09/24 19:48:52 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+
+	public:
+		ScavTrap();
+		ScavTrap( const std::string &name );
+		ScavTrap( ScavTrap &scavTrap );
+
+		// Member functions
+		void	attack( std::string const &target );
+		void	guardGate( void );
+		
+		~ScavTrap();
+
+};
+
+#endif
