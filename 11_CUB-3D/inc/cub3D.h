7f797acfb5dbcbee4a652ec1f2ea3894bedab13d
Petr Simcak <psimcak@1-D-24.42heilbronn.de>
2024-10-29 15:09:11 +0100

    Cub3D - first files and MLX42

---
+++ 11_CUB-3D/inc/cub3D.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 13:53:48 by psimcak           #+#    #+#             */
+/*   Updated: 2024/10/29 14:36:04 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <mlx.h>		//
+# include <math.h>		//
+# include <fcntl.h>		// O_RDONLY, open
+# include <unistd.h>	// read, write, close
+# include <stdio.h>		// printf, perror
+# include <stdlib.h>	// malloc, free, exit, +_SUCCESS, +_FAILURE
+# include <string.h>	// strerror
+
+// Colors
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define BLUE		"\033[34m"
+# define YELLOW		"\033[33m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define RST		"\033[0m"
+
+
+
+#endif
