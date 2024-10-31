19aa3ed7ac4a6ccecbe257aab4f9708e8ae8b8ed
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-10-31 21:20:41 +0100

    Cub3D - fixing mlx and compilation errors

--- 11_CUB-3D/algorithm/src/utils/safe_exit.c
+++ 11_CUB-3D/algorithm/src/utils/safe_exit.c
@@ -6,13 +6,13 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 20:32:36 by psimcak           #+#    #+#             */
-/*   Updated: 2024/10/29 20:54:38 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/10/31 20:33:58 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
 
-void	safe_exit(t_game *game, char *msg)
+void	safe_exit(t_game *game, const char *msg)
 {
 	safe_free(game);
 	perror(msg);
