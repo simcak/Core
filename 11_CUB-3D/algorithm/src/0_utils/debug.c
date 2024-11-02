ea6a74957e261177c53eaa095a456f255846f558
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-11-02 19:02:01 +0100

    Cub3D - argument checker

--- 11_CUB-3D/algorithm/src/0_utils/debug.c
+++ 11_CUB-3D/algorithm/src/0_utils/debug.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 15:59:30 by psimcak           #+#    #+#             */
-/*   Updated: 2024/11/02 16:19:13 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/11/02 16:31:10 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	ft_debug(int type, t_main *game)
 	if (type == ALL)
 	{
 		printf(BR"DEBUG\n"RST);
-		printf("game->mlx->width: %d\n", game->mlx->width);
+		printf("game->map->fd = %d\n", game->map->fd);
 	}
 }
