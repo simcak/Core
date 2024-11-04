e5061875db53c9d37b455daa14d60354a357447f
Petr Simcak <psimcak@2-C-1.42heilbronn.de>
2024-11-04 11:52:31 +0100

    Cub3D - gnl added + some func comments + parser extended

--- 11_CUB-3D/algorithm/main.c
+++ 11_CUB-3D/algorithm/main.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 13:53:51 by psimcak           #+#    #+#             */
-/*   Updated: 2024/11/02 16:31:45 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/11/04 09:40:42 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int ac, char **av)
 
 	game = ft_safe_malloc(sizeof(t_main), ERR_MALL_GAME);
 	init(DEFAULT, game, ac, av);
+	parser(game);
 	// ft_debug(ALL, game);
 	// init(MLX, game, 0, NULL);
 	safe_free(game);
--- 11_CUB-3D/algorithm/src/0_utils/ft_str_utils.c
+++ 11_CUB-3D/algorithm/src/0_utils/ft_str_utils.c
@@ -6,12 +6,15 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:19:56 by psimcak           #+#    #+#             */
-/*   Updated: 2024/11/02 16:21:51 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/11/04 09:56:37 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
+/**
+ * @brief Calculate the length of a string
+ */
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -22,6 +25,10 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+/**
+ * @brief Compare two strings
+ * If the strings are equal, the function returns 0.
+ */
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
