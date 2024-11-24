5281f2508ab7a5dc72bb70969cb4a41b1d214832
Petr Simcak <78808411+simcak@users.noreply.github.com>
2024-11-24 15:21:55 +0100

    Cub3D - small step forward rey casting + small init modification and debug edit

--- 11_CUB-3D/algorithm/src/0_utils/safe_free.c
+++ 11_CUB-3D/algorithm/src/0_utils/safe_free.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 20:33:42 by psimcak           #+#    #+#             */
-/*   Updated: 2024/11/23 19:58:33 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/11/24 13:10:40 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,52 @@ int	free_str_arr(char **arr)
 	return (EXIT_SUCCESS);
 }
 
-int	free_txt(t_txt *txt)
+static void	free_mlx(t_main *game)
 {
-	if (txt->paths)
-		free(txt->paths);
-	if (txt->mlx_txt_no)
-		mlx_delete_texture(txt->mlx_txt_no);
-	if (txt->mlx_txt_so)
-		mlx_delete_texture(txt->mlx_txt_so);
-	if (txt->mlx_txt_we)
-		mlx_delete_texture(txt->mlx_txt_we);
-	if (txt->mlx_txt_ea)
-		mlx_delete_texture(txt->mlx_txt_ea);
-	free(txt);
-	return (EXIT_SUCCESS);
+	if (game->mlx)
+	{
+		if (game->image)
+			mlx_delete_image(game->mlx, game->image);
+		mlx_close_window(game->mlx);
+		mlx_terminate(game->mlx);
+	}
+	return ;
 }
 
-int	free_color(t_color *color)
+static void	free_txt(t_txt *txt)
 {
-	if (color->colors)
-		free(color->colors);
-	if (color->rgb_raw)
-		free_str_arr(color->rgb_raw);
-	if (color->rgb_c)
-		free(color->rgb_c);
-	if (color->rgb_f)
-		free(color->rgb_f);
-	free(color);
-	return (EXIT_SUCCESS);
+	if (txt)
+	{
+		if (txt->paths)
+			free(txt->paths);
+		if (txt->mlx_txt_no)
+			mlx_delete_texture(txt->mlx_txt_no);
+		if (txt->mlx_txt_so)
+			mlx_delete_texture(txt->mlx_txt_so);
+		if (txt->mlx_txt_we)
+			mlx_delete_texture(txt->mlx_txt_we);
+		if (txt->mlx_txt_ea)
+			mlx_delete_texture(txt->mlx_txt_ea);
+		free(txt);
+	}
+	return ;
+}
+
+static void	free_color(t_color *color)
+{
+	if (color)
+	{
+		if (color->colors)
+			free(color->colors);
+		if (color->rgb_raw)
+			free_str_arr(color->rgb_raw);
+		if (color->rgb_c)
+			free(color->rgb_c);
+		if (color->rgb_f)
+			free(color->rgb_f);
+		free(color);
+	}
+	return ;
 }
 
 /**
@@ -60,35 +78,25 @@ int	free_color(t_color *color)
  */
 void	free_full(t_main *game)
 {
-	if (game->mlx)
-	{
-		mlx_close_window(game->mlx);
-		mlx_terminate(game->mlx);
-		if (game->image)
-			mlx_delete_image(game->mlx, game->image);
-		// mlx_delete_image(game->mlx, game->animation);
-		// mlx_delete_texture(game->animation_txt);
-	}
-	if (game->player)
-		free(game->player);
-	if (game->ray)
-		free(game->ray);
-	if (game->file)
-	{
-		if (game->file->parsed_file)
-			free_str_arr(game->file->parsed_file);
-		free(game->file);
-	}
-	if (game->file->txt)
-		free_txt(game->file->txt);
-	if (game->file->color)
-		free_color(game->file->color);
+	free_mlx(game);
+	free_txt(game->file->txt);
+	free_color(game->file->color);
 	if (game->file->map)
 	{
 		if (game->file->map->grid)
 			free_str_arr(game->file->map->grid);
 		free(game->file->map);
 	}
+	if (game->file)
+	{
+		if (game->file->parsed_file)
+			free_str_arr(game->file->parsed_file);
+		free(game->file);
+	}
+	if (game->player)
+		free(game->player);
+	if (game->ray)
+		free(game->ray);
 	if (game)
 		free(game);
 }
--- 11_CUB-3D/algorithm/src/1_init/1_init_default.c
+++ 11_CUB-3D/algorithm/src/1_init/1_init_default.c
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:09:30 by psimcak           #+#    #+#             */
-/*   Updated: 2024/11/23 19:43:54 by psimcak          ###   ########.fr       */
+/*   Updated: 2024/11/24 15:02:31 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ static void	init_default_player(t_main *game)
 	game->player->dir_y = 0;
 	game->player->cam_x = 0;
 	game->player->cam_y = 0;
+	game->player->move_speed = 0;
+	game->player->rot_speed = 0;
 	game->player->buff_dist = 0;
 	game->player->aspect_ratio = 0;
 	game->player->step_x = 0;
@@ -78,18 +80,40 @@ static void init_default_map(t_file *file)
 }
 
 /**
+ * @brief Checks the arguments validity.
+ * 
+ * 1) Checks the number of arguments.
+ * 2) Checks the file extension.
+ * 3) Checks the file descriptor.
+ * 
+ * If there is some problem, the program exits with an error message.
+ */
+static void	argument_checker(t_main *game)
+{
+	int		len;
+
+	if (game->ac != 2)
+		safe_exit(game, BR"Input has to be: \"./cub3d <map.cub>\""RST);
+	len = ft_strlen(game->av[1]);
+	if (ft_strncmp(&game->av[1][len - 4], ".cub", 4) != 0)
+		safe_exit(game, BR"Input file has to be \".cub\""RST);
+	game->file->fd = open(game->av[1], O_RDONLY);
+	if (game->file->fd < 0)
+		safe_exit(game, BR"Invalid file"RST);
+}
+
+/**
  * @brief Initialize default game struct
  */
 void	init_default(t_main *game)
 {
 	game->mlx = NULL;
 	game->image = NULL;
-	game->animation = NULL;
-	game->animation_txt = NULL;
 	init_default_player(game);
 	init_default_ray(game);
 	init_default_file(game);
 	init_default_txt(game->file);
 	init_default_color(game->file);
 	init_default_map(game->file);
+	argument_checker(game);
 }
