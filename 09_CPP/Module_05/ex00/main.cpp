dfc36a1f1b5908892e13f8cea976c07057bbf0fc
Petr <peta.simcak@gmail.com>
2025-02-03 15:25:14 +0100

    gitignore chages mainly

--- 09_CPP/Module_05/ex00/main.cpp
+++ 09_CPP/Module_05/ex00/main.cpp
@@ -6,14 +6,15 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 13:39:29 by psimcak           #+#    #+#             */
-/*   Updated: 2025/01/20 16:20:31 by psimcak          ###   ########.fr       */
+/*   Updated: 2025/01/23 15:21:10 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
-	Bureaucrat *bureaucrat = new Bureaucrat("John", 1);
+	Bureaucrat	*bureaucrat = new Bureaucrat("John", 1);
+
 	std::cout << *bureaucrat << std::endl;
 
 	delete bureaucrat;
