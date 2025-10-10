0f8145e474b3a1429d8cb008a16f5fb447fb5616
Petr Simcak <78808411+simcak@users.noreply.github.com>
2025-10-10 13:04:12 +0200

    small-edit

--- 09_CPP/Module_07/ex00/main.cpp
+++ 09_CPP/Module_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:43:56 by psimcak           #+#    #+#             */
-/*   Updated: 2025/10/10 13:01:45 by psimcak          ###   ########.fr       */
+/*   Updated: 2025/10/10 13:04:01 by psimcak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,3 +54,24 @@ int main( void ) {
 
 	return 0;
 }
+
+/* subject-main */
+// int main( void ) {
+// 	int a = 2;
+// 	int b = 3;
+
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+// 	std::string c = "chaine1";
+// 	std::string d = "chaine2";
+
+// 	::swap(c, d);
+// 	std::cout << "c = " << c << ", d = " << d << std::endl;
+// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+// 	return 0;
+// }
