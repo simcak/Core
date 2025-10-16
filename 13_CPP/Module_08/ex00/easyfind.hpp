0fd5c5d96d8e0b7b879f4dfbbe7be570900a2372
Petr Simcak <78808411+simcak@users.noreply.github.com>
2025-10-16 17:28:56 +0200

    module08-template

---
+++ 13_CPP/Module_08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 17:01:47 by psimcak           #+#    #+#             */
+/*   Updated: 2025/10/16 17:26:02 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+/**
+ * @brief In a `container` of integers find the `match`
+*/
+template <typename T> void	easyfind(T &container, int match) {
+	
+};
+
+#endif
