2885fe6c0a9151e553e3e7e600557b379b39e155
Petr Simcak <78808411+simcak@users.noreply.github.com>
2025-12-01 18:33:50 +0100

    testing

--- 13_CPP/Module_08/ex01/main.cpp
+++ 13_CPP/Module_08/ex01/main.cpp
@@ -2,19 +2,58 @@
 
 int	main( void )
 {
+	std::cout << BY "Subject Test" RST << std::endl;
 	{
-		Span sp = Span(5);
+		try
+		{
+			Span sp = Span(5);
+			
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
 
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+	std::cout << BY "\nMy Test 1" RST << std::endl;
+	{
+		try
+		{
+			Span sp = Span(8);
+			
+			sp.addNumber(3);
+			sp.addNumber(1);
+			sp.addNumber(-4);
+			sp.addNumber(1);
+			sp.addNumber(5);
+			sp.addNumber(9);
+			sp.addNumber(-9);
+			sp.addNumber(12);
+			// sp.addNumber(12);
+			
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 
-		return 0;
+	std::cout << BY "\nMy Test 2" RST << std::endl;
+	{
+		Span sp_r = Span(20000);
+		sp_r.addNumber(20001, time(NULL));
+		std::cout << sp_r.shortestSpan() << std::endl;
+		std::cout << sp_r.longestSpan() << std::endl;
 	}
 
 	return 0;
