eb453e9d84684fe6bd1b97b6b8d777a9fb603e03
Petr Simcak <78808411+simcak@users.noreply.github.com>
2025-12-05 15:33:36 +0100

    implemented-iteration

--- 13_CPP/Module_08/ex02/MutantStack.hpp
+++ 13_CPP/Module_08/ex02/MutantStack.hpp
@@ -19,15 +19,30 @@ class MutantStack : public std::stack<T>
 {
 
 	public:
+		typedef typename std::stack<T>::container_type	c_type;
+		typedef typename c_type::iterator				iterator;
+		typedef typename c_type::const_iterator			const_iterator;
+		typedef typename c_type::reverse_iterator		reverse_iterator;
+		typedef typename c_type::const_reverse_iterator	const_reverse_iterator;
+
 		/* ───────────────────── Orthodox Canonical Form ──────────────────── */
 		MutantStack();
 		MutantStack(const MutantStack &copy);
 		MutantStack	&operator=(const MutantStack &);
 		~MutantStack();
 
-		/* ──────────────────────── member functions ──────────────────────── */
+		/* ──────────────── member functions -iterator access ─────────────── */
+		iterator				begin()  { return this->c.begin(); }
+		iterator				end()    { return this->c.end(); }
+		reverse_iterator		rbegin() { return this->c.rbegin(); }
+		reverse_iterator		rend()   { return this->c.rend(); }
+		const_iterator			begin() const  { return this->c.begin(); }
+		const_iterator			end() const    { return this->c.end(); }
+		const_reverse_iterator	rbegin() const { return this->c.rbegin(); }
+		const_reverse_iterator	rend() const   { return this->c.rend(); }
 
 };
 
+#include "MutantStack.tpp"
 
 #endif
