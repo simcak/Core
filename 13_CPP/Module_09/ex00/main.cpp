2b236135e7660ff54e75e1cd27a12d87627fede1
Petr Simcak <78808411+simcak@users.noreply.github.com>
2025-12-15 22:58:18 +0100

    better

--- 13_CPP/Module_09/ex00/main.cpp
+++ 13_CPP/Module_09/ex00/main.cpp
@@ -1,28 +1,18 @@
 #include "BitcoinExchange.hpp"
 
-void	_print_err(int key, char **argv)
-{
-	if (key == ERR_ARGS)
-		std::cout << BRERR "Correct usage: <program> <data>" << std::endl;
-	else if (key == ERR_OPEN)
-		std::cout << BRERR "Couldn't open the file '" << argv[1] << "'\n";
-	else if (key == ERR_MPTY)
-		std::cout << BRERR "No content in the file '" << argv[1] << "'\n";
-}
-
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-		return (_print_err(ERR_ARGS, argv), 1);
+		return (std::cout << BRERR "Correct usage: <program> <data>\n", 1);
 
-	std::ifstream inputFile(argv[1]);
+	try
+	{
+		BitcoinExchange history(argv[1]);
+	}
+	catch(const std::exception& e)
+	{
+		return (std::cerr << e.what() << '\n', 1);
+	}
 
-	if (!inputFile.is_open())
-		return (_print_err(ERR_OPEN, argv), 1);
-	if (inputFile.peek() == std::ifstream::traits_type::eof())
-		return (_print_err(ERR_MPTY, argv), 1);
-
-	BitcoinExchange history(argv[1]);
-
-	return (inputFile.close(), 0);
+	return 0;
 }
