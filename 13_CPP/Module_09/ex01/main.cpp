483983cb5afa6774a9fb390eafac7427dc89999f
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-06 18:51:46 +0100

    test-ready

--- 13_CPP/Module_09/ex01/main.cpp
+++ 13_CPP/Module_09/ex01/main.cpp
@@ -1,6 +1,26 @@
 #include "RPN.hpp"
 
-int main()
-{
+#define USAGE	BG "Usage:" RST " ./RPN \"8 9 * 9 - 9 - 9 - 4 - 1 +\"\n"
+#define ALLOWED	BRERR "Error: Only numbers, operators, space are accepted.\n"
 
+
+int main(int argc, char **argv)
+{
+	std::string	input;
+
+	if (argc != 2)
+		return (std::cout << USAGE, 1);
+
+	input = argv[1];
+	if (input.find_first_not_of("0123456789+-*/ ") != std::string::npos)
+		return (std::cout << ALLOWED, 1);
+
+	try {
+		RPN obj1(input);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
 }
