6ded677db7fc9c88ebeda97bdcd3d2928607a5ba
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-06 18:51:16 +0100

    template

--- 13_CPP/Module_09/ex01/RPN.cpp
+++ 13_CPP/Module_09/ex01/RPN.cpp
@@ -3,6 +3,17 @@
 /* ───────────────────────── Orthodox Canonical Form ──────────────────────── */
 RPN::RPN() {}
 
+RPN::RPN(const std::string input) {}
+
+RPN::RPN(const RPN &copy) : _container(copy._container) {}
+
+RPN &RPN::operator=(const RPN &other)
+{
+	if (this != &other)
+		_container = other._container;
+	return *this;
+}
+
 RPN::~RPN() {}
 
 /* ───────────────────────────────── getters ──────────────────────────────── */
@@ -10,3 +21,6 @@ RPN::~RPN() {}
 /* ──────────────────────────── member functions ──────────────────────────── */
 
 /* ──────────────────────────────── exception ─────────────────────────────── */
+const char *RPN::placeholder::what() const throw() {
+	return BREXC "...";
+}
