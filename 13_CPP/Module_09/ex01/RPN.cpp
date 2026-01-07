cf8b889ebc686b91cd7269cfcd6d0aee65148124
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-07 15:56:16 +0100

    working-hopefully-fine

--- 13_CPP/Module_09/ex01/RPN.cpp
+++ 13_CPP/Module_09/ex01/RPN.cpp
@@ -1,26 +1,91 @@
 #include "RPN.hpp"
 
 /* ───────────────────────── Orthodox Canonical Form ──────────────────────── */
-RPN::RPN() {}
+RPN::RPN() : _result(0) {}
 
-RPN::RPN(const std::string input) {}
+RPN::RPN(const std::string &input) : _result(0)
+{
+	std::istringstream	iss(input);
+	std::string			tok;
 
-RPN::RPN(const RPN &copy) : _container(copy._container) {}
+	while (iss >> tok)
+		processToken(tok);
+
+	if (_stack.size() != 1)
+		throw NotOneOutput();
+
+	_result = _stack.top();
+}
+
+RPN::RPN(const RPN &other) : _stack(other._stack), _result(other._result) {}
 
 RPN &RPN::operator=(const RPN &other)
 {
 	if (this != &other)
-		_container = other._container;
+	{
+		_stack = other._stack;
+		_result = other._result;
+	}
 	return *this;
 }
 
 RPN::~RPN() {}
 
-/* ───────────────────────────────── getters ──────────────────────────────── */
+/* ──────────────────────────────── getters ──────────────────────────────── */
+long RPN::getResult() const { return _result; }
 
-/* ──────────────────────────── member functions ──────────────────────────── */
+/* ──────────────────────────── member functions ─────────────────────────── */
+void RPN::processToken(const std::string &tok)
+{
+	if (tok.size() != 1)
+		throw tokenLength();
 
-/* ──────────────────────────────── exception ─────────────────────────────── */
-const char *RPN::placeholder::what() const throw() {
-	return BREXC "...";
+	char tok_c = tok[0];
+
+	if (std::isdigit(tok_c))
+		return (_stack.push(static_cast<long>(tok_c - '0')));
+
+	if (tok_c == '+' || tok_c == '-' || tok_c == '*' || tok_c == '/')
+		return (applyOperator(tok_c));
+
+	throw invalidChar();
+}
+
+void RPN::applyOperator(char op)
+{
+	if (_stack.size() < 2)
+		throw notEnoughNums();
+
+	long b = _stack.top(); _stack.pop();
+	long a = _stack.top(); _stack.pop();
+
+	if (op == '+')
+		_stack.push(a + b);
+	else if (op == '-')
+		_stack.push(a - b);
+	else if (op == '*')
+		_stack.push(a * b);
+	else
+		(b == 0) ? throw zeroDivision() : _stack.push(a / b);
+}
+
+/* ──────────────────────────────── exception ────────────────────────────── */
+const char *RPN::invalidChar::what() const throw() {
+	return (BRERR "Only numbers, operators, space are accepted.");
+}
+
+const char *RPN::tokenLength::what() const throw() {
+	return (BRERR "Token must be exactly 1 char: either digit or operator.");
+}
+
+const char *RPN::notEnoughNums::what() const throw() {
+	return (BRERR "Operator needs 2 RPN expressions.");
+}
+
+const char *RPN::zeroDivision::what() const throw() {
+	return (BRERR "You tried to devide by zero!");
+}
+
+const char *RPN::NotOneOutput::what() const throw() {
+	return (BRERR "You have to end with 1! RPN expression.");
 }
