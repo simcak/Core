a2a2e44b91454b509bfc625683a4579bec03278f
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-07 16:06:38 +0100

    first-draft

---
+++ 13_CPP/Module_09/ex02/PmergeMe.hpp
@@ -0,0 +1,43 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+/* ───────────────────────────────── COLORS ───────────────────────────────── */
+#define BR		"\033[1;31m"
+#define BG		"\033[1;32m"
+#define BY		"\033[1;33m"
+#define RST		"\033[0m"
+
+#define BRERR	BR "Error: " RST
+#define BREXC	BR "Exception: " RST
+
+/* ───────────────────────────────── CLASSES ──────────────────────────────── */
+class PmergeMe
+{
+
+	private:
+		/* data */
+
+	public:
+		/* ───────────────────── Orthodox Canonical Form ──────────────────── */
+		PmergeMe();
+		~PmergeMe();
+
+		/* ──────────────────────── member function ───────────────────────── */
+		
+		/* ──────────────────────────── getters ───────────────────────────── */
+
+		/* ─────────────────────────── exception ──────────────────────────── */
+
+};
+
+PmergeMe::PmergeMe(/* args */)
+{
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+
+
+#endif
