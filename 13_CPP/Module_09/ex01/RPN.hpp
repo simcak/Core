e6797569f854aea9e48c4ed913df6f1da60b46c0
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-07 15:10:15 +0100

    template-2-practical-stucture

--- 13_CPP/Module_09/ex01/RPN.hpp
+++ 13_CPP/Module_09/ex01/RPN.hpp
@@ -4,11 +4,9 @@
 #include <iostream>
 #include <stack>
 #include <exception>
-
-enum ErrorCode
-{
-	
-};
+#include <string>
+#include <sstream>
+#include <cctype>
 
 /* ───────────────────────────────── COLORS ───────────────────────────────── */
 #define BR		"\033[1;31m"
@@ -24,23 +22,48 @@ class RPN
 {
 
 	private:
-		std::stack<std::string>	_container;
+		std::stack<long>	_stack;
+		long				_result;
+
+		/* ──────────────────────── member function ───────────────────────── */
+		void	processToken(const std::string &tok);
+		void	applyOperator(char op);
 
 	public:
 		/* ───────────────────── Orthodox Canonical Form ──────────────────── */
 		RPN();
-		RPN(const std::string input);
+		RPN(const std::string &input);
 		RPN(const RPN &copy);
 		RPN &operator=(const RPN &);
 		~RPN();
 
 		/* ──────────────────────────── getters ───────────────────────────── */
-
-		/* ──────────────────────── member function ───────────────────────── */
+		long	getResult() const;
 
 		/* ─────────────────────────── exception ──────────────────────────── */
-		class placeholder : public std::exception {
-			public: const char*		what() const throw();
+		class invalidChar : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class tokenLength : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class notEnoughNums : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class zeroDivision : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class NotOneOutput : public std::exception {
+			public:
+				const char *what() const throw();
 		};
 };
 
