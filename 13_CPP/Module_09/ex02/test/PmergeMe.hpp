2d330872e313e51409bd1a2b405025fc86495b3a
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-15 20:44:34 +0100

    same, but with count of comperations

---
+++ 13_CPP/Module_09/ex02/test/PmergeMe.hpp
@@ -0,0 +1,68 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <sstream>
+
+#define	BEFORE	1
+#define	AFTER	2
+
+/* ───────────────────────────────── COLORS ───────────────────────────────── */
+#define BR		"\033[1;31m"
+#define BG		"\033[1;32m"
+#define BY		"\033[1;33m"
+#define RST		"\033[0m"
+
+#define BRERR	BR "Error: " RST
+#define BREXC	BR "Exception: " RST
+
+/* ───────────────────────────────── CLASSES ──────────────────────────────── */
+class PmergeMe
+{
+
+	private:
+		std::vector<int>	_vector;
+		std::deque<int>		_deque;
+
+	public:
+		static size_t	nbr_of_comps;
+
+		template<typename L, typename R>
+		static bool	countedLess(const L &lhs, const R &rhs) {
+			++nbr_of_comps;
+			return lhs < rhs;
+		}
+
+		/* ───────────────────── Orthodox Canonical Form ──────────────────── */
+		PmergeMe();
+		PmergeMe(const std::string input);
+		PmergeMe(const PmergeMe &copy);
+		PmergeMe	&operator=(const PmergeMe &other);
+		~PmergeMe();
+
+		/* ──────────────────────── member function ───────────────────────── */
+		void	sort();
+
+		/* ─────────────────────────── exception ──────────────────────────── */
+		class Limit : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class DiffRes : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+		class EmptyArg : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+
+};
+
+
+#endif
