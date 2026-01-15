1eba2444d16cffaf113a6f14c526212157e01d68
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-15 15:31:17 +0100

    modulable-run.sh

--- 13_CPP/Module_09/ex02/run.sh
+++ 13_CPP/Module_09/ex02/run.sh
@@ -4,6 +4,8 @@ numbers=$(shuf -i 1-${1:-10000} -n ${1:-4200})
 
 numbers_str=$(echo $numbers | tr '\n' ' ')
 
-# echo $numbers_str
+if [ "${2:-0}" == "1" ]; then
+	echo -e "$numbers_str\n"
+fi
 
 ./PmergeMe $numbers_str
