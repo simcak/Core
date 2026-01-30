ccad0678d94c664cd02776e1dd1b2e4f1e30bd76
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-01-30 17:14:42 +0100

    simpleChannelRules

--- sources/classes/Channel.cpp
+++ sources/classes/Channel.cpp
@@ -1,69 +1,63 @@
-#include "./IRC.hpp"
-#include "./Channel.hpp"
-#include "./User.hpp"
+#include "Channel.hpp"
+#include "User.hpp"
 
 /* ───────────────────────────── Con/Des-tructor ──────────────────────────── */
 Channel::Channel(std::string const &name) :
-	_name(name),
-	_topic(""),
-	_users(),
-	_banList(),
-	_operators(),
-	_password(),
-	_user_limit(9)
+	_name(name), _topic(""), _users(), _banList(), _operators(), _password()
 {
-	INFO("Channel <" << _name << "> has been created");
+	CST("Channel <" << _name << "> has been created");
 }
 
-Channel::~Channel(){ INFO("Channel <" << _name << "> has been detroyed"); }
-
-/* ───────────────────────────────── Getters ──────────────────────────────── */
-std::string	Channel::getUserName() const { return _name; }
-std::string	Channel::getTopic() const { return _topic; }
+Channel::~Channel() { DST("Channel <" << _name << "> has been destroyed"); }
 
 /* ──────────────────────────── Member functions ──────────────────────────── */
 bool	Channel::isUserInChanel(User *user) const
 {
-	size_t	numOfUsers = _users.size();
-
-	for(size_t i = 0; i < numOfUsers; ++i)
+	for (size_t i = 0; i < _users.size(); ++i)
 		if (_users[i] == user)
-			return (true);
-
-	return (false);
+			return true;
+	return false;
 }
 
-bool	Channel::isUserBanned(User *user) const {
-	size_t	numOfBanned = _banList.size();
-
-	for(size_t i = 0; i < numOfBanned; ++i)
+bool	Channel::isUserBanned(User *user) const
+{
+	for (size_t i = 0; i < _banList.size(); ++i)
 		if (_banList[i] == user)
-			return (true);
-
-	return (false);
+			return true;
+	return false;
 }
 
-// Checks if user is already in the channel
-// If not, adds them to the _users vector
-// Returns *this for method chaining
 Channel	&Channel::addUser(User *user)
 {
-	if (!this->isUserInChanel(user))
+	if (!user)
+		return *this;
+	if (!isUserInChanel(user))
 		_users.push_back(user);
 	return *this;
 }
 
-Channel	&Channel::setTopic(const std::string &topic)
+Channel	&Channel::removeUser(User *user)
 {
-	_topic = topic;
-	return (*this);
+	if (!user)
+		return *this;
+
+	std::vector<User *>::iterator	it = _users.begin();
+	for (; it != _users.end(); ++it)
+	{
+		if (*it == user)
+		{
+			_users.erase(it);
+			return *this;
+		}
+	}
+	return *this;
 }
 
-// Checks if user is already banned
-// If not, adds them to the _banList vector
 Channel	&Channel::banUser(User *user)
 {
-	if (!this->isUserBanned(user))
+	if (!user)
+		return *this;
+	if (!isUserBanned(user))
 		_banList.push_back(user);
 	return *this;
 }
@@ -71,17 +65,15 @@ Channel	&Channel::banUser(User *user)
 Channel	&Channel::unBanUser(User *user)
 {
 	std::vector<User *>::iterator	it = _banList.begin();
-
 	for (; it != _banList.end(); ++it)
 	{
 		if (*it == user)
 		{
-			DEBG("UnBan: found banned user <" 
-					<< (*it)->getUserName() << ">. Unbanning");
+			DEBG("UnBan: found banned user <" << user->getUserName() << ">. Unbanning");
 			_banList.erase(it);
 			return *this;
 		}
 	}
-	WARN("UnBan: user <" << user->getUserName()  << "> not found in ban list");
+	WARN("UnBan: user <" << (user ? user->getUserName() : "NULL") << "> not found in ban list");
 	return *this;
 }
