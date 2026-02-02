7191d6e860fd952b60dd313306a547c3214c6d19
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-02-02 11:13:45 +0100

    comments+formatting

--- sources/utils/utils.cpp
+++ sources/utils/utils.cpp
@@ -1,32 +1,37 @@
 #include "../classes/IRC.hpp"
 
-bool	OnlyDigits(const std::string &str)
+/**
+ * @brief Check if a string contains only digit characters
+ * @param str Input string to check
+ * @return true if the string contains only digits, false otherwise
+ */
+bool	onlyDigits(const std::string &str)
 {
-	for (size_t i = 0; i < str.length(); ++i)
-		if (!isdigit(static_cast<unsigned char>(str[i])))
+	if (str.empty())
+		return false;
+	for (size_t i = 0; i < str.size(); ++i)
+		if (!std::isdigit(static_cast<unsigned char>(str[i])))
 			return false;
-
-	return !str.empty();
+	return true;
 }
 
+/**
+ * @brief Get current date and time as a formatted string "YYYY-MM-DD HH:MM:SS"
+ * @return Formatted date-time string
+ */
 std::string	getCurrentDateTime(void)
 {
 	time_t		rawtime;
 	struct tm	timeinfo;
 
-	// Get current time
 	time(&rawtime);
 
-	// Copy localtime safely
-	#if defined(_WIN32) || defined(_WIN64)
-		localtime_s(&timeinfo, &rawtime);
-	#else
-		struct tm* tmp = localtime(&rawtime);
-		if (tmp != NULL)
-			timeinfo = *tmp;
-	#endif
+	struct tm *tmp = localtime(&rawtime);
+	if (tmp)
+		timeinfo = *tmp;
+	else
+		std::memset(&timeinfo, 0, sizeof(timeinfo));
 
-	// Format: YYYY-MM-DD HH:MM:SS
 	std::ostringstream oss;
 	oss << (1900 + timeinfo.tm_year) << "-"
 		<< std::setw(2) << std::setfill('0') << (timeinfo.tm_mon + 1) << "-"
@@ -38,18 +43,23 @@ std::string	getCurrentDateTime(void)
 	return oss.str();
 }
 
-
+/**
+ * @brief Check if a TCP port is already in use.
+ * @param port Port number to check
+ * @return true if the port is in use, false otherwise
+ */
 bool	isPortInUse(int port)
 {
-	int	sock = socket(AF_INET, SOCK_STREAM, 0);
-	int	opt = 1;
+	int	sock = socket(AF_INET, SOCK_STREAM, 0), opt = 1;
 
 	if (sock < 0)
 		return true;
+
 	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
 	struct sockaddr_in	addr;
 
+	std::memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(port);
@@ -57,9 +67,9 @@ bool	isPortInUse(int port)
 	if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
 	{
 		close(sock);
-		return true; // port is in use
+		return true;
 	}
 
 	close(sock);
-	return false; // port is free
+	return false;
 }
