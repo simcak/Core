09afe59159353d4f6d5a7518ef14a1ca1b2d70af
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-02-02 11:21:18 +0100

    template

---
+++ sources/commands/CmdSendMsg.cpp
@@ -0,0 +1,12 @@
+#include "../classes/IRC.hpp"
+#include "../classes/Server.hpp" //full definition needed
+#include "../classes/User.hpp"
+#include "../classes/Channel.hpp"
+
+/**
+ * Command: SEND
+ * Parameters: <filename> <target>
+ * 
+ * SEND command is used for a file transferring via server storage.
+ */
+ 
