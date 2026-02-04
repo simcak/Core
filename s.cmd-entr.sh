7b4e7f0db7d048a7a5ae03f802c317cb0291575f
Petr Simcak <78808411+simcak@users.noreply.github.com>
2026-02-04 21:33:27 +0100

    shell-removed

--- s.cmd-entr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DEBUG_FLAG="-DDEBUG=1"
-
-make re && ./ircserv 6697 h
--- s.entr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while true; do
-	find . \( -name \*.hpp -o -name \*.cpp \)  | entr -cr bash command.entr.sh 
-done
