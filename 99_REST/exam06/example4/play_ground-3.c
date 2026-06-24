76ca97abd53d254311200921edf1930e68f9ae85
Petr Šimčák <peta.simcak@gmail.com>
2026-06-24 22:30:53 +0200

    round-4

---
+++ 99_REST/exam06/example4/play_ground-3.c
@@ -0,0 +1,182 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int		count = 0, max_fd = 0;
+int		ids[88888];
+char	*msgs[88888];
+
+fd_set	wfds, rfds, afds;
+char	buf_read[88888], buf_write[42];
+
+
+int	extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int		i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = -1;
+	while ((*buf)[++i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+char	*str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+void	fatal_error()
+{
+	write(2, "Fatal error\n", 12);
+	exit(1);
+}
+
+void	notify_other(int author, char *str)
+{
+	for (int fd = 0; fd <= max_fd; fd++)
+		if (fd != author && FD_ISSET(fd, &wfds))
+			send(fd, str, strlen(str), 0);
+}
+
+void	register_client(int fd)
+{
+	max_fd = fd > max_fd ? fd : max_fd;
+	ids[fd] = count++;
+	msgs[fd] = NULL;
+	FD_SET(fd, &afds);
+	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
+	notify_other(fd, buf_write);
+}
+
+void	remove_client(int fd)
+{
+	sprintf(buf_write, "server: client %d just left\n", ids[fd]);
+	notify_other(fd, buf_write);
+	free(msgs[fd]);
+	FD_CLR(fd, &afds);
+	close(fd);
+}
+
+void	send_msg(int fd)
+{
+	char	*msg;
+
+	while (extract_message(&(msgs[fd]), &msg))
+	{
+		sprintf(buf_write, "client %d: ", ids[fd]);
+		notify_other(fd, buf_write);
+		notify_other(fd, msg);
+		free(msg);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int					sockfd, connfd;
+	struct sockaddr_in	servaddr, cli;
+
+	if (ac != 2) { return(write(2, "Wrong number of arguments\n", 26), 1); }
+	FD_ZERO(&afds);
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1]));
+
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n");
+	if (listen(sockfd, 10) != 0) {
+		printf("cannot listen\n"); 
+		exit(0); 
+	}
+	max_fd = sockfd;
+	FD_SET(max_fd, &afds);
+
+	while (1)
+	{
+		rfds = wfds = afds;
+
+		if (select(max_fd + 1, &rfds, &wfds, NULL, NULL) == -1)
+			fatal_error();
+
+		for (int fd = 0; fd <= max_fd; fd++)
+		{
+			if (!FD_ISSET(fd, &rfds))
+				continue;
+			if (fd == sockfd)
+			{
+				socklen_t	addr_len = sizeof(servaddr);
+				connfd = accept(sockfd, (struct sockaddr *)&cli, &addr_len);
+				if (connfd >= 0)
+				{
+					register_client(connfd);
+					break;
+				}
+			}
+			else
+			{
+				int	bytes_read = recv(fd, buf_read, 8888, 0);
+				if (bytes_read <= 0)
+				{
+					remove_client(fd);
+					break;
+				}
+				buf_read[bytes_read] = 0;
+				msgs[fd] = str_join(msgs[fd], buf_read);
+				send_msg(fd);
+			}
+		}
+	}
+	return (0);
+}
